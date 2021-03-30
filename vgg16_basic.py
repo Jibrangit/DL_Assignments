d9e47f3056a30ba379bfb21e0197cf06120be218
Jibran <73052263+Jibrangit@users.noreply.github.com>
2021-03-30 14:08:25 -0400

    Add files via upload

---
+++ vgg16_basic.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+y_load = np.load('/home/jibran/Desktop/DL_Assignments/HW6/facesAndAges/ages.npy')
+x_load = np.load('/home/jibran/Desktop/DL_Assignments/HW6/facesAndAges/faces.npy')
+
